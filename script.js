214d662cf9335a4ceaac4bf3b8f8133b281f8876
Richard Metzner <metznerrichard245@student.voskh.cz>
2025-10-20 12:00:37 +0200

    let a, b, c

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const tbxKoefA = document.getElementById("tbxKoefA");
 const tbxKoefB = document.getElementById("tbxKoefB");
 const tbxKoefC = document.getElementById("tbxKoefC");
 const txaResult = document.getElementById("txaResult");
+
+function korenyRovnice() {
+    let a = Number(tbxKoefA.value);
+    let b = Number(tbxKoefB.value);
+    let c = Number(tbxKoefC.value);
+}
