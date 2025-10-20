33a538b765f11888a5200057927349d05936f3dd
Richard Metzner <metznerrichard245@student.voskh.cz>
2025-10-20 12:11:25 +0200

    Expression

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ function korenyRovnice() {
     if (a!=0) {
         // řešení kvadratické rovnice
         let D = b*b-4*a*c;
+        // let D = Math.pow(b,2)-4*a*c;
+        if (D==0) {
+            let x = -b/(2*a);
+
+        }
+        else {
+
+        }
 
     }
     else {
