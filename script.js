49d37d41ebcfc300b8971961de9f91e1d89a3e15
Richard Metzner <metznerrichard245@student.voskh.cz>
2025-10-20 11:59:41 +0200

    const

---
+++ script.js
@@ -0,0 +1,4 @@
+const tbxKoefA = document.getElementById("tbxKoefA");
+const tbxKoefB = document.getElementById("tbxKoefB");
+const tbxKoefC = document.getElementById("tbxKoefC");
+const txaResult = document.getElementById("txaResult");
